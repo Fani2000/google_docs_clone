c57004fac002b9491e43751646c0d2a54022da3e
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-07-18 22:10:55 +0200

    The Project is done, more features coming soon

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 import "quill/dist/quill.snow.css";
 import "../styles.css";
 
@@ -15,6 +16,7 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+  const { id: documentId } = useParams();
   const [socket, setSocket] = useState(null);
   const [quill, setQuill] = useState(null);
 
@@ -26,6 +28,7 @@ const TextEditor = () => {
       s.disconnect();
     };
   }, []);
+
   useEffect(() => {
     if (socket == null || quill == null) return;
     const handler = (delta) => {
@@ -53,6 +56,27 @@ const TextEditor = () => {
     };
   }, [quill, socket]);
 
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    socket.emit("get-document", documentId);
+  }, [quill, socket, documentId]);
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
 
@@ -65,6 +89,8 @@ const TextEditor = () => {
         toolbar: TOOLBAR_OPTIONS,
       },
     });
+    q.disable();
+    q.setText("Loading....");
     setQuill(q);
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,38 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
 const io = require("socket.io")(3001, {
   cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
 });
 
+const defaultValue = "";
+mongoose.connect("mongodb://localhost/google-docs-clone");
+
 io.on("connection", (socket) => {
-  console.log("Connection established");
-  socket.on("send-changes", (delta) => {
-    socket.broadcast.emit("receive-changes", delta);
+  console.log("Connection established", socket.id);
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
   });
 });
 
 io.on("disconnect", () => {
   console.log("Disconnection");
 });
+
+const findOrCreateDocument = async (id) => {
+  if (id === null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+};
